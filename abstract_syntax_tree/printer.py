a46a319e370b692610bff91e101971b4efd8e16c
Æ Griffiths <164574794+damwaingames@users.noreply.github.com>
2024-07-17 08:50:20 +0100

    Rewrote printer using visitor pattern

--- abstract_syntax_tree/printer.py
+++ abstract_syntax_tree/printer.py
@@ -10,104 +10,87 @@ from .expressions import (
     Unary,
     Variable,
 )
-from .statements import Stmt, Block, Expression, If, Print, Var
+from .statements import Stmt, Block, Expression, If, Print, Var, While
 from scanner.token import Token
 
 
-class ASTPrinter:
-    def print_stmt(self, statement: Stmt) -> str:
-        match statement:
-            case Block():
-                return self._parenthesize2("block", *statement._statements)
-            case Expression():
-                return self._parenthesize(";", statement._expression)
-            case If():
-                if statement._else_branch:
-                    return self._parenthesize2(
-                        "if-else",
-                        statement._condition,
-                        statement._then_branch,
-                        statement._else_branch,
-                    )
-                return self._parenthesize2(
-                    "if", statement._condition, statement._then_branch
-                )
-            case Print():
-                return self._parenthesize("print", statement._expression)
-            case Var():
-                if statement._initializer is None:
-                    return self._parenthesize2("var", statement._name)
-                return self._parenthesize2(
-                    "var", statement._name, "=", statement._initializer
-                )
-            case _:
-                raise NotImplementedError(
-                    f"Not added a case for a certain statement : {statement}"
-                )
+class ASTPrinter(Expr.Visitor, Stmt.Visitor):
+    def print(self, x: Expr | Stmt) -> str:
+        return x.accept(self)
 
-    def print_expr(self, expression: Expr) -> str:
-        match expression:
-            case Assign():
-                return self._parenthesize2(expression._name.lexeme, expression._value)
-            case Binary():
-                return self._parenthesize(
-                    expression._operator.lexeme, expression._left, expression._right
-                )
-            case Grouping():
-                return self._parenthesize("group", expression._expression)
-            case Literal():
-                match expression._value:
-                    case None:
-                        return "nil"
-                    case bool():
-                        return str(expression._value).lower()
-                    case _:
-                        return str(expression._value)
-            case Logical():
-                return self._parenthesize(
-                    expression._operator.lexeme, expression._left, expression._right
-                )
-            case Unary():
-                return self._parenthesize(
-                    expression._operator.lexeme, expression._right
-                )
-            case Variable():
-                return expression._name.lexeme
-            case _:
-                raise NotImplementedError(
-                    f"Not added a case for a certain expression : {expression}"
-                )
+    def visit_block_stmt(self, stmt: Block) -> Any:
+        builder = "(block "
+        for statement in stmt._statements:
+            builder += statement.accept(self)
+        builder += ")"
+        return builder
 
-    def _parenthesize(self, name: str, *args: Expr) -> str:
-        output = f"({name}"
-        for expression in args:
-            output += " "
-            output += self.print_expr(expression)
-        output += ")"
-        return output
+    def visit_expression_stmt(self, stmt: Expression) -> Any:
+        return self._parenthesize(";", stmt._expression)
+
+    def visit_if_stmt(self, stmt: If) -> Any:
+        if stmt._else_branch is None:
+            return self._parenthesize2("if", stmt._condition, stmt._then_branch)
+        return self._parenthesize2(
+            "if-else", stmt._condition, stmt._then_branch, stmt._else_branch
+        )
+
+    def visit_print_stmt(self, stmt: Print) -> Any:
+        return self._parenthesize("print", stmt._expression)
+
+    def visit_var_stmt(self, stmt: Var) -> Any:
+        if stmt._initializer is None:
+            return self._parenthesize2("var", stmt._name)
+        return self._parenthesize2("var", stmt._name, "=", stmt._initializer)
+
+    def visit_while_stmt(self, stmt: While) -> Any:
+        return self._parenthesize2("while", stmt._condition, stmt._body)
+
+    def visit_assign_expr(self, expr: Assign) -> Any:
+        return self._parenthesize2("=", expr._name.lexeme, expr._value)
+
+    def visit_binary_expr(self, expr: Binary) -> Any:
+        return self._parenthesize(expr._operator.lexeme, expr._left, expr._right)
+
+    def visit_grouping_expr(self, expr: Grouping) -> Any:
+        return self._parenthesize("group", expr._expression)
+
+    def visit_literal_expr(self, expr: Literal) -> Any:
+        return "nil" if expr._value is None else str(expr._value)
+
+    def visit_logical_expr(self, expr: Logical) -> Any:
+        return self._parenthesize(expr._operator.lexeme, expr._left, expr._right)
+
+    def visit_unary_expr(self, expr: Unary) -> Any:
+        return self._parenthesize(expr._operator.lexeme, expr._right)
+
+    def visit_variable_expr(self, expr: Variable) -> Any:
+        return expr._name.lexeme
+
+    def _parenthesize(self, name: str, *exprs: Expr) -> str:
+        builder = f"({name}"
+        for expr in exprs:
+            builder += " "
+            builder += expr.accept(self)
+        builder += ")"
+        return builder
 
     def _parenthesize2(self, name: str, *parts: Any) -> str:
-        output = f"({name}"
-        output = self._transform(output, parts)
-        output += ")"
-        return output
+        builder = f"({name}"
+        builder = self._transform(builder, parts)
+        builder += ")"
+        return builder
 
-    def _transform(self, output: str, *parts: Any) -> str:
+    def _transform(self, builder: str, *parts: Any) -> str:
         for part in parts:
-            output += " "
+            builder += " "
             match part:
-                case Expr():
-                    output += self.print_expr(part)
-                case Stmt():
-                    output += self.print_stmt(part)
+                case Expr() | Stmt():
+                    builder += part.accept(self)
                 case Token():
-                    output += part.lexeme
+                    builder += part.lexeme
                 case tuple():
-                    output += self._transform(output, *part)
-                case str():
-                    output += part
+                    builder += self._transform(builder, *part)
                 case _:
-                    raise NotImplementedError(
-                        f"Not added a case for a certain expression or statement : {part}"
-                    )
-        return output
+                    builder += part
+        return builder
